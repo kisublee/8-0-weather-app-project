571af5ee47b4612db67d1f40ead443565f5713a1
kisublee <yigi2021@gmail.com>
2021-10-09 23:05:51 -0400

    Add searches to .history & remove the default msg when new search is being made.

--- main.js
+++ main.js
@@ -32,6 +32,27 @@ const weatherApp = document.querySelector("form").addEventListener("submit", (ev
         <p>Country:</b> ${db.nearest_area[0].country[0].value}</p>
         <p>Currently:</b> Feels Like ${db.current_condition[0].FeelsLikeF}°F</p>`
 
+
+        const li = document.createElement("li")
+        li.innerHTML =  
+        `<a href="#" value='${basicUrl} name='test'>
+        ${db.nearest_area[0].areaName[0].value}- 
+        ${db.current_condition[0].FeelsLikeF}°F</a>`;
+        
+        const list = document.querySelector(".history ul");
+     
+        let found = false;
+        document.querySelectorAll("ul li").forEach((each) => {
+          if (each.innerHTML === li.innerHTML) {
+            found = true;
+          }
+        });
+        if (!found) {
+          list.append(li);
+        }
+        
+        // remove ul's default text when a search is being made
+        document.querySelector(".history ul p").textContent = ""
     
         }).catch(console.log)
 
