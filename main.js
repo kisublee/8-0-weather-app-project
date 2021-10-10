9026c57133dc736e8e429e73001e6692bcce920e
kisublee <yigi2021@gmail.com>
2021-10-09 23:24:23 -0400

    Pass the first and second test. There are some minor issues though to handle.

--- main.js
+++ main.js
@@ -1,28 +1,20 @@
 
-
 const weatherApp = document.querySelector("form").addEventListener("submit", (event) => {
 
     event.preventDefault();
-
     //input value
     const input = event.target.search.value;
-
-    //Basic URL
+    //fetch the url
     const basicUrl = `https://wttr.in/${input}?format=j1`
-
     //Toronto
     //Area: Toronto
     //Region: Ontario
     //Country: Canada
     //Currently: Feels like 92F
-
-    //If there is no input, return an error message.
     if (!input) {
        document.querySelector(".display").textContent = "Please enter a city or location"
        throw "error"
     } 
-
-    //Fetch
     fetch(basicUrl).then((response) => response.json()).then((db) => {
         
         document.querySelector(".display").innerHTML = 
@@ -32,13 +24,36 @@ const weatherApp = document.querySelector("form").addEventListener("submit", (ev
         <p>Country:</b> ${db.nearest_area[0].country[0].value}</p>
         <p>Currently:</b> Feels Like ${db.current_condition[0].FeelsLikeF}°F</p>`
 
-
         const li = document.createElement("li")
         li.innerHTML =  
         `<a href="#" value='${basicUrl} name='test'>
         ${db.nearest_area[0].areaName[0].value}- 
         ${db.current_condition[0].FeelsLikeF}°F</a>`;
         
+        li.addEventListener("click", (event) => {
+ 
+            document.querySelector(".display").innerHTML = 
+            `<h2><b>${input}</b></h2>
+            <p><b>Area:</b> ${db.nearest_area[0].areaName[0].value}</p>
+            <p>Region:</b> ${db.nearest_area[0].region[0].value}</p>
+            <p>Country:</b> ${db.nearest_area[0].country[0].value}</p>
+            <p>Currently:</b> Feels Like ${db.current_condition[0].FeelsLikeF}°F</p>`
+
+            const days = ["today", "tomorrow", "dayAfterTomorrow"];
+            for (let i = 0; i < days.length; i++) {
+    
+                const div = document.createElement("div")
+                div.innerHTML = `
+                <p><b>Average Temperature:</b> ${db.weather[i].avgtempF}°F</p>
+                <p><b>Max Temperature:</b> ${db.weather[i].maxtempF}°F</p>
+                <p><b>Min Temperature:</b> ${db.weather[i].mintempF}°F</p>`
+    
+                  document.querySelector(".display").append(div)  
+            }
+           
+ 
+         })
+
         const list = document.querySelector(".history ul");
      
         let found = false;
@@ -50,11 +65,23 @@ const weatherApp = document.querySelector("form").addEventListener("submit", (ev
         if (!found) {
           list.append(li);
         }
-        
-        // remove ul's default text when a search is being made
+
         document.querySelector(".history ul p").textContent = ""
-    
-        }).catch(console.log)
+
+        const days = ["today", "tomorrow", "dayAfterTomorrow"];
+        for (let i = 0; i < days.length; i++) {
+
+            const div = document.createElement("div")
+            div.innerHTML = `
+            <p><b>Average Temperature:</b> ${db.weather[i].avgtempF}°F</p>
+            <p><b>Max Temperature:</b> ${db.weather[i].maxtempF}°F</p>
+            <p><b>Min Temperature:</b> ${db.weather[i].mintempF}°F</p>`
+
+              document.querySelector(".display").append(div)  
+        }
+       
+
+    }).catch(console.log)
 
     event.target.reset();
 
